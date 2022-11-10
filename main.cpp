727efc04a00e81134758b009d387030e0ab8c38b
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-11-10 09:11:04 -0500

    added length example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
-
+#include <cmath>
+#include <iomanip>
+#include "coord3d.h"
 #include "funcs.h"
 
 int main()
 {
+  // Task A
+  Coord3D pointP = {10, 20, 30};
+  std::cout << "Task A\n\n" << length(&pointP) << std::endl;
+  std::cout << "---------------------------------------\n";
+  
   return 0;
 }
