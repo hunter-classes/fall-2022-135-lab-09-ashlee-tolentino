5c9f8f1d3d8507f8c69630d8528fd504e478e765
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-11-10 09:07:10 -0500

    task a done

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,22 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab9
+
+This program...
+*/
+
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "coord3d.h"
 #include "funcs.h"
 
-// add functions here
-
+// Task A: This function receives the coordinates of a point P passed as a pointer, and computes the distance from the origin to the point P
+double length(Coord3D *p)
+{
+  double result = sqrt((p->x * p->x) + (p->y * p->y) + (p->z * p->z));
+  //double result = p->z;
+  return result;
+}
